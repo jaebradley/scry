a0cb107f08df30000a03a05762ee06cda671c9c1
Jae Bradley <jae.b.bradley@gmail.com>
2024-08-26 22:10:00 -0400

    Update

--- tests/services/firstparty/zillow.test.ts
+++ tests/services/firstparty/zillow.test.ts
@@ -3,6 +3,9 @@ import GeocodeClient from "../../../src/data/thirdparty/googlemaps/client";
 import {Client as GoogleMapsClient} from "@googlemaps/google-maps-services-js";
 import { describe, test, expect } from "vitest";
 import {getSearchPageResponse} from "../../../src/data/thirdparty/zillow/client";
+import { stringify } from "csv-stringify";
+import { generate } from "csv-generate";
+import * as fs from "node:fs";
 
 describe('OrderedPropertiesCalculator', () => {
     describe('Get properties by largest difference between listing and estimated prices', () => {
@@ -16,7 +19,11 @@ describe('OrderedPropertiesCalculator', () => {
            const results = await calculator.getPropertiesOrderedByLargestDifferenceBetweenListingAndEstimatedPrice('Cambridge, MA');
            expect(results).toBeTruthy();
            expect(results.length).greaterThan(0);
-           console.log(JSON.stringify(results.map(v => ({ id: v.id, listingPrice: v.price, zestimate: v.zestimate, address: v.address }))));
+
+           // const file = fs.createWriteStream('./cambridge.csv')
+           // generate()
+           //     .pipe(stringify(results.map(v => ({ 'Zillow ID': v.id, 'Listing Price': v.unformattedPrice, 'Zestimate': v.zestimate, 'Address': v.address })), { header: true}))
+           //     .pipe(file);
         });
     })
 })
