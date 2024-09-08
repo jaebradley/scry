205c22977fc2531bc4c94fc4615a7c0f93c0f8d7
Jae Bradley <jae.b.bradley@gmail.com>
2024-09-08 18:45:30 -0400

    Integrate live data (#8)

--- packages/frontend/app/page.tsx
+++ packages/frontend/app/page.tsx
@@ -1,44 +1,33 @@
-'use client';
-
 import OrderedPropertiesCalculator from "../../backend/src/services/firstparty/zillow";
 import GeocodeClient from "../../backend/src/data/firstparty/googlemaps/client";
 import {getSearchPageResponse} from "../../backend/src/data/thirdparty/zillow/client";
 import {Column} from "@/components/Table/Row";
 import Table from "@/components/Table";
 
-export default function Home() {
-    // const apiKey = process?.env?.GOOGLE_MAPS_API_KEY;
-    // if (!apiKey) {
-    //     throw new Error("Missing apiKey");
-    // }
-    //
-    // const calculator = new OrderedPropertiesCalculator(
-    //     new GeocodeClient(apiKey),
-    //     getSearchPageResponse
-    // );
-    // TODO: @jaebradley use real data in future
-    // const results = await calculator.getPropertiesOrderedByLargestDifferenceBetweenListingAndEstimatedPrice('Cambridge, MA');
-    const results = [
-        {
-            identifier: "jae",
-            valuesByColumn: {
-                [Column.Address]: "foo",
-                [Column.Price]: 123_456,
-                [Column.Estimate]: 100_000,
-                [Column.Difference]: 100,
-            },
-        },
-        {
-            identifier: "baebae",
-            valuesByColumn: {
-                [Column.Address]: "bar",
-                [Column.Price]: 456_789,
-                [Column.Estimate]: 500_000,
-                [Column.Difference]: 1000,
-            }
-        }
-    ];
+const apiKey = process?.env?.GOOGLE_MAPS_API_KEY;
+if (!apiKey) {
+    throw new Error("Missing apiKey");
+}
 
+const calculator = new OrderedPropertiesCalculator(
+    new GeocodeClient(apiKey),
+    getSearchPageResponse
+);
+
+export default async function Home() {
+
+    const results = await calculator.getPropertiesOrderedByLargestDifferenceBetweenListingAndEstimatedPrice('Cambridge, MA');
+
+    const propertyData = results
+        .map(v => ({
+            identifier: v.id,
+            valuesByColumn: {
+                [Column.Address]: v.address,
+                [Column.Price]: v.unformattedPrice,
+                [Column.Estimate]: v.zestimate,
+                [Column.Difference]: v.zestimate - v.unformattedPrice
+            }
+        }))
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -52,19 +41,7 @@ export default function Home() {
                         [Column.Difference, "Difference"],
                     ])
                 }
-                propertyData={results}
-                comparatorsByColumn={{
-                    [Column.Address]: (a: string, b: string) => a.localeCompare(b),
-                    [Column.Price]: (a: number, b: number) => a - b,
-                    [Column.Estimate]: (a: number, b: number) => a - b,
-                    [Column.Difference]: (a: number, b: number) => a - b,
-                }}
-                formattersByColumn={{
-                    [Column.Address]: (v: string) => v,
-                    [Column.Price]: (v: number) => v.toLocaleString("en-US", { style: "currency", currency: "USD", maximumFractionDigits: 0 }),
-                    [Column.Estimate]: (v: number) => v.toLocaleString("en-US", { style: "currency", currency: "USD", maximumFractionDigits: 0 }),
-                    [Column.Difference]: (v: number) => v.toLocaleString("en-US", { style: "currency", currency: "USD", maximumFractionDigits: 0 }),
-                }}
+                propertyData={propertyData}
             />
         </main>
     );
