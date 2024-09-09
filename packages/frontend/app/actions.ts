e6856eea08e2893b33f48ee364842a3b52f99898
Jae Bradley <jae.b.bradley@gmail.com>
2024-09-09 17:16:07 -0400

    Location to specific page (#9)

---
+++ packages/frontend/app/actions.ts
@@ -0,0 +1,7 @@
+'use server';
+
+import { redirect } from "next/navigation";
+
+export async function navigateToPropertiesPage(data: FormData) {
+    redirect(`/properties/${data.get('location')}`);
+}
