b5d13b85b6b2d337bee4ef93c50257b3e5588595
Jae Bradley <jae.b.bradley@gmail.com>
2024-09-08 21:07:25 -0400

    Location to specific page

--- packages/frontend/app/page.tsx
+++ packages/frontend/app/page.tsx
@@ -1,48 +1,15 @@
-import OrderedPropertiesCalculator from "../../backend/src/services/firstparty/zillow";
-import GeocodeClient from "../../backend/src/data/firstparty/googlemaps/client";
-import {getSearchPageResponse} from "../../backend/src/data/thirdparty/zillow/client";
-import {Column} from "@/components/Table/Row";
-import Table from "@/components/Table";
+'use client';
 
-const apiKey = process?.env?.GOOGLE_MAPS_API_KEY;
-if (!apiKey) {
-    throw new Error("Missing apiKey");
-}
+import { navigateToPropertiesPage } from  './actions';
 
-const calculator = new OrderedPropertiesCalculator(
-    new GeocodeClient(apiKey),
-    getSearchPageResponse
-);
-
-export default async function Home() {
-
-    const results = await calculator.getPropertiesOrderedByLargestDifferenceBetweenListingAndEstimatedPrice('Cambridge, MA');
-
-    const propertyData = results
-        .map(v => ({
-            identifier: v.id,
-            valuesByColumn: {
-                [Column.Address]: v.address,
-                [Column.Price]: v.unformattedPrice,
-                [Column.Estimate]: v.zestimate,
-                [Column.Difference]: v.zestimate - v.unformattedPrice
-            }
-        }))
+export default function Home() {
 
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <Table
-                orderedColumns={[Column.Address, Column.Price, Column.Estimate, Column.Difference]}
-                namesByColumn={
-                    new Map([
-                        [Column.Address, "Address"],
-                        [Column.Price, "Listing Price"],
-                        [Column.Estimate, "Zillow Estimate"],
-                        [Column.Difference, "Difference"],
-                    ])
-                }
-                propertyData={propertyData}
-            />
+        <main>
+            <form action={navigateToPropertiesPage}>
+                <input name="location" type="text" placeholder="Specify a location to search for properties..."/>
+                <button type="submit">Search</button>
+            </form>
         </main>
     );
 }
---
+++ packages/frontend/app/properties/[location]/page.tsx
@@ -0,0 +1,47 @@
+import OrderedPropertiesCalculator from "../../../../backend/src/services/firstparty/zillow";
+import GeocodeClient from "../../../../backend/src/data/firstparty/googlemaps/client";
+import {getSearchPageResponse} from "../../../../backend/src/data/thirdparty/zillow/client";
+import {Column} from "@/components/Table/Row";
+import Table from "@/components/Table";
+
+const apiKey = process?.env?.GOOGLE_MAPS_API_KEY;
+if (!apiKey) {
+    throw new Error("Missing apiKey");
+}
+
+const calculator = new OrderedPropertiesCalculator(
+    new GeocodeClient(apiKey),
+    getSearchPageResponse
+);
+
+export default async function Page({ params }: { params: { location: string } }) {
+    const results = await calculator.getPropertiesOrderedByLargestDifferenceBetweenListingAndEstimatedPrice(params.location);
+
+    const propertyData = results
+        .map(v => ({
+            identifier: v.id,
+            valuesByColumn: {
+                [Column.Address]: v.address,
+                [Column.Price]: v.unformattedPrice,
+                [Column.Estimate]: v.zestimate,
+                [Column.Difference]: v.zestimate - v.unformattedPrice
+            }
+        }))
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <Table
+                orderedColumns={[Column.Address, Column.Price, Column.Estimate, Column.Difference]}
+                namesByColumn={
+                    new Map([
+                        [Column.Address, "Address"],
+                        [Column.Price, "Listing Price"],
+                        [Column.Estimate, "Zillow Estimate"],
+                        [Column.Difference, "Difference"],
+                    ])
+                }
+                propertyData={propertyData}
+            />
+        </main>
+    );
+}
